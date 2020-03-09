b6d40f225a83dd6255831fa3ec4adacf1d26c752
ConnorHartland <hartlanddevelop@gmail.com>
2020-03-09 16:55:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ rooms = {
         }
     }
 inventory = []
-    currentRoom = 'Entrance'
+currentRoom = 'Entrance'
 
-    showInstructions()
+showInstructions()
 
-    while True:
+while True:
 
-        showStatus()
+	showStatus()
 
-        move = ''
-        while move == '':
-            move = input('>')
+	move = ''
+	while move == '':
+		move = input('>')
 
-        move = move.lower().split()
+	move = move.lower().split()
 
-        if move[0] == 'run':
-            print('You choose to run.')
+	if move[0] == 'run':
+		print('You choose to run.')
 
 
-        elif "item" in rooms[currentRoom] and move[0] == "take":
-            #add the item to their inventory
-            inventory += rooms[currentRoom]['item'].join(' ')
-            #display a helpful message
-            print(move[1] + ' got!')
-            #delete the item from the room
-            del rooms[currentRoom]['item']
-        else:
-            print('Does not work like that.')
+	elif "item" in rooms[currentRoom] and move[0] == "take":
+		#add the item to their inventory
+		inventory += rooms[currentRoom]['item'].join(' ')
+		#display a helpful message
+		print(move[1] + ' got!')
+		#delete the item from the room
+		del rooms[currentRoom]['item']
+	else:
+		print('Does not work like that.')
